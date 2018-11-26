06773683c5c6b82c72788ebce46e2f29726899a2
luciano <lputruele@gmail.com>
2018-11-26 14:02:03 -0300

    fixed saturation bug

--- src/graph/ExplicitCompositeModel.java
+++ src/graph/ExplicitCompositeModel.java
@@ -96,8 +96,7 @@ public class ExplicitCompositeModel {
 		Pair transition = new Pair(from,to);
 		if (labels.get(transition) == null)
 			return false;
-
-		return succList.get(from).contains(to) && preList.get(to).contains(from) && labels.get(transition).contains(lbl);
+		return  labels.get(transition).contains(lbl);
 	}
 
 
